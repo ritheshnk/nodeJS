3fea2ff9f1d8d100612e5d0c220df26f6d43b1f3
nkrithesh <nkrithesh@gmail.com>
2023-07-10 17:10:30 +0530

    second commit

--- intro/index.js
+++ intro/index.js
@@ -1,9 +1,9 @@
 const fs= require("fs");
 
-// fs.writeFile("greetings.txt","Hello Rithesh",(err)=>{
-// //     if(err) throw err;
-// //     console.log("the file is saved sucessfully");
-// // });
+fs.writeFile("greetings.txt","Hello Rithesh",(err)=>{
+    if(err) throw err;
+    console.log("the file is saved sucessfully");
+});
 
 fs.readFile("greetings.txt","utf-8",(err,data) => {
     if(err) throw err;
